2f14204b46ae0a6294405ba153e4432b8020237f
Reut Bibi <82286345+reutbibi@users.noreply.github.com>
2022-12-08 22:05:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("find-me").addEventListener("click", geoFindMe);
+    document.getElementById("shareBtn").addEventListener("click", share);
+
+    var linktoShare = "";
+    var titletoShare = "";
+
+    function geoFindMe() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+            document.getElementById("status").textContent = "The browser does not support GeoLocation";
+        }
+
+        function success(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            linktoShare=`https://maps.google.com/?q=${latitude},${longitude}`;
+            titletoShare=`https://maps.google.com/?output=embed&q=${latitude},${longitude}`;
+            
+            document.getElementById("map-link").textContent = linktoShare;
+            document.getElementById("iframe").src = titletoShare;
+            document.getElementById("iframe").classList.remove("d-none");
+        }
+
+        function error() {
+            document.getElementById("status").textContent = "The browser could not reteive GeoLocation";
+        }
+
+    }
+
+    function share() {
+        if (navigator.share) {
+            const shareData = {
+                title: "My Geo Location",
+                text: titletoShare,
+                url: linktoShare,
+            }
+            navigator.share(shareData);
+        } else 
+        {
+            document.getElementById("status").textContent = "Could not share data...";
+        }
+    }
+})
